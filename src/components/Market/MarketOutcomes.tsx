f7237769f91fda849959a49f4caedc28868b2fa9
Wellington Matheus <w.matheus@outlook.com>
2022-04-12 10:15:25 -0300

    refactor(MarketOutcomes): move outcome state logic to upper level

--- src/components/Market/MarketOutcomes.tsx
+++ src/components/Market/MarketOutcomes.tsx
@@ -1,4 +1,4 @@
-import { useEffect } from 'react';
+import { useCallback, useEffect, useMemo } from 'react';
 
 import classNames from 'classnames';
 import { fromPriceChartToLineChartSeries } from 'helpers/chart';
@@ -30,42 +30,40 @@ const outcomeStates = {
 
 type MarketOutcomesItemProps = {
   market: Market;
-  outcome: Outcome;
+  outcome: {
+    id: string | number;
+    title: string;
+    state: {
+      isDefault: boolean;
+      isSuccess: boolean;
+      isDanger: boolean;
+      isActive: boolean;
+    };
+    price: {
+      isPriceUp: boolean;
+      value: string;
+      lastWeekPricesChartSeries: any;
+    };
+    result: {
+      isResolved: boolean;
+      state: {
+        isWon: boolean;
+        isLoss: boolean;
+        isVoided: boolean;
+      };
+    };
+  };
 };
 
 function MarketOutcomesItem({ market, outcome }: MarketOutcomesItemProps) {
   const dispatch = useAppDispatch();
-  const selectedOutcomeId = useAppSelector(
-    state => state.trade.selectedOutcomeId
-  );
-  const selectedMarketId = useAppSelector(
-    state => state.trade.selectedMarketId
-  );
-
-  const { id, marketId, title, price } = outcome;
-
-  const isCurrentSelectedPrediction =
-    marketId === selectedMarketId && id === selectedOutcomeId;
-
-  const isMarketResolved = market.state === 'resolved';
-  const isVoided = market.voided;
-  const isWinningOutcome = isMarketResolved && market.resolvedOutcomeId === id;
+  const { title, state, price, result } = outcome;
 
   useEffect(() => {
-    if (isCurrentSelectedPrediction) {
+    if (state.isActive) {
       dispatch(openTradeForm());
     }
-  }, [dispatch, isCurrentSelectedPrediction]);
-
-  // using 7d timeframe
-  const marketPriceChart = outcome.priceCharts.find(
-    priceChart => priceChart.timeframe === '7d'
-  );
-  const marketPriceUp =
-    !marketPriceChart?.changePercent || marketPriceChart?.changePercent > 0;
-  const chartData = fromPriceChartToLineChartSeries(
-    marketPriceChart?.prices || []
-  );
+  }, [dispatch, state.isActive]);
 
   function handleItemSelection() {
     if (market.state === 'closed') {
@@ -75,7 +73,7 @@ function MarketOutcomesItem({ market, outcome }: MarketOutcomesItemProps) {
     }
     dispatch(marketSelected(market));
 
-    if (!isCurrentSelectedPrediction) {
+    if (!state.isActive) {
       dispatch(selectOutcome(market.id, outcome.id));
     } else {
       dispatch(selectOutcome(market.id, ''));
@@ -87,12 +85,12 @@ function MarketOutcomesItem({ market, outcome }: MarketOutcomesItemProps) {
     <button
       type="button"
       className={classNames({
-        'pm-c-market-outcomes__item--default': !isMarketResolved,
-        'pm-c-market-outcomes__item--success': isWinningOutcome,
-        'pm-c-market-outcomes__item--danger': !isWinningOutcome,
-        active: isCurrentSelectedPrediction
+        'pm-c-market-outcomes__item--default': state.isDefault,
+        'pm-c-market-outcomes__item--success': state.isSuccess,
+        'pm-c-market-outcomes__item--danger': state.isDanger,
+        active: state.isActive
       })}
-      disabled={isMarketResolved}
+      disabled={result.isResolved}
       onClick={handleItemSelection}
     >
       <div className="pm-c-market-outcomes__item-group--column">
@@ -119,24 +117,24 @@ function MarketOutcomesItem({ market, outcome }: MarketOutcomesItemProps) {
             fontWeight="medium"
             className="pm-c-market-outcomes__item-value"
           >
-            {price.toFixed(3)}
+            {price.value}
           </Text>
-          {!isMarketResolved ? (
-            <>{marketPriceUp ? <ArrowUpIcon /> : <ArrowDownIcon />}</>
+          {!result.isResolved ? (
+            <>{price.isPriceUp ? <ArrowUpIcon /> : <ArrowDownIcon />}</>
           ) : null}
         </div>
       </div>
-      {isMarketResolved ? (
+      {result.isResolved ? (
         <div className="pm-c-market-outcomes__item-result">
-          {isWinningOutcome && !isVoided ? outcomeStates.success.icon : null}
-          {!isWinningOutcome && !isVoided ? outcomeStates.danger.icon : null}
-          {isVoided ? outcomeStates.voided.icon : null}
+          {result.state.isWon ? outcomeStates.success.icon : null}
+          {result.state.isLoss ? outcomeStates.danger.icon : null}
+          {result.state.isVoided ? outcomeStates.voided.icon : null}
         </div>
       ) : (
         <div className="pm-c-market-outcomes__item-chart">
           <MiniAreaChart
-            serie={chartData}
-            color={marketPriceUp ? 'success' : 'danger'}
+            serie={price.lastWeekPricesChartSeries}
+            color={price.isPriceUp ? 'success' : 'danger'}
             width={48}
           />
         </div>
@@ -150,10 +148,74 @@ type MarketOutcomesProps = {
 };
 
 function MarketOutcomes({ market }: MarketOutcomesProps) {
-  const { outcomes } = market;
+  const { outcomes, resolvedOutcomeId } = market;
+
+  const { selectedMarketId, selectedOutcomeId } = useAppSelector(
+    state => state.trade
+  );
+
+  const isMarketResolved = market.state === 'resolved';
+  const isMarketVoided = market.voided;
+
+  const buildOutcomeObject = useCallback(
+    (outcome: Outcome) => {
+      // States
+      const isSelectedOutcome =
+        market.id === selectedMarketId && outcome.id === selectedOutcomeId;
+      const isWinningOutcome =
+        isMarketResolved && resolvedOutcomeId === outcome.id;
+
+      // Prices
+      const lastWeekPrices = outcome.priceCharts.find(
+        priceChart => priceChart.timeframe === '7d'
+      );
+      const lastWeekPricesChartSeries = fromPriceChartToLineChartSeries(
+        lastWeekPrices?.prices || []
+      );
+      const isPriceUp =
+        !lastWeekPrices?.changePercent || lastWeekPrices?.changePercent > 0;
+
+      return {
+        id: outcome.id,
+        title: outcome.title,
+        state: {
+          isDefault: !isMarketResolved,
+          isSuccess: isWinningOutcome,
+          isDanger: !isWinningOutcome,
+          isActive: isSelectedOutcome
+        },
+        price: {
+          isPriceUp,
+          value: outcome.price.toFixed(3),
+          lastWeekPricesChartSeries
+        },
+        result: {
+          isResolved: isMarketResolved,
+          state: {
+            isWon: isWinningOutcome && !isMarketVoided,
+            isLoss: !isWinningOutcome && !isMarketVoided,
+            isVoided: isMarketVoided
+          }
+        }
+      };
+    },
+    [
+      isMarketResolved,
+      isMarketVoided,
+      market.id,
+      resolvedOutcomeId,
+      selectedMarketId,
+      selectedOutcomeId
+    ]
+  );
+
+  const outcomesObjects = useMemo(() => {
+    return outcomes.map(outcome => buildOutcomeObject(outcome));
+  }, [buildOutcomeObject, outcomes]);
+
   return (
     <ul className="pm-c-market-outcomes">
-      {outcomes.map(outcome => (
+      {outcomesObjects.map(outcome => (
         <li key={outcome.id}>
           <MarketOutcomesItem market={market} outcome={outcome} />
         </li>
