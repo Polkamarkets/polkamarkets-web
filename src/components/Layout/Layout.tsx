91d344cb86268bb64ee5ed87977dad0a16759531
soujvnunes <16447765+soujvnunes@users.noreply.github.com>
2022-11-23 21:14:36 -0300

    feat: adjust homer header ui response

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -1,23 +1,28 @@
-import { Container } from 'ui';
-
 import BetaWarning from '../BetaWarning';
 import Footer from '../Footer';
 import NavBar from '../NavBar';
 import RightSidebar from '../RightSidebar';
+import ScrollableArea from '../ScrollableArea';
 
 export default function Layout({ children }: React.PropsWithChildren<{}>) {
   return (
-    <>
-      <NavBar />
-      <Container as="main" className="pm-l-layout__main">
-        {children}
-        <footer className="pm-l-layout__footer">
-          <Footer />
-        </footer>
-      </Container>
-      <RightSidebar />
+    <div className="pm-l-layout">
       <BetaWarning />
+      <NavBar />
+      <ScrollableArea className="pm-l-layout__scrollable-area">
+        <main className="pm-l-layout__main">
+          {children}
+          <footer className="pm-l-layout__footer">
+            <Footer />
+          </footer>
+        </main>
+      </ScrollableArea>
+      <ScrollableArea>
+        <aside className="pm-l-layout__aside">
+          <RightSidebar />
+        </aside>
+      </ScrollableArea>
       <div id="toast-notification-portal" />
-    </>
+    </div>
   );
 }
