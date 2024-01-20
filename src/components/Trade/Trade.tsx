765fa4d0104c65f1cf677b303ec917f60d4e1e9a
Victor <soujvnunes@gmail.com>
2024-01-20 08:18:38 -0300

    feat(Trade): adjust sell warning

--- src/components/Trade/Trade.tsx
+++ src/components/Trade/Trade.tsx
@@ -1,13 +1,16 @@
-import { CSSProperties } from 'react';
+import { CSSProperties, useCallback, useMemo } from 'react';
 
 import cn from 'classnames';
 import { features } from 'config';
 import getMarketColors from 'helpers/getMarketColors';
+import { changeTradeType, selectOutcome } from 'redux/ducks/trade';
 import { useTheme } from 'ui';
 
-import { useAppSelector } from 'hooks';
+import { useAppDispatch, useAppSelector, useLanguage } from 'hooks';
 
+import { AlertMini } from '../Alert';
 import Breadcrumb from '../Breadcrumb';
+import { Button } from '../Button';
 import TradeFormClosed from '../TradeForm/TradeFormClosed';
 import TradeFormInput from '../TradeForm/TradeFormInput';
 import { views } from './Trade.config';
@@ -23,10 +26,20 @@ type TradeProps = {
 };
 
 function Trade({ view = 'default', onTradeFinished }: TradeProps) {
+  const dispatch = useAppDispatch();
   const theme = useTheme();
+  const language = useLanguage();
   const market = useAppSelector(state => state.market.market);
   const isLoadingMarket = useAppSelector(state => state.market.isLoading);
 
+  const type = useAppSelector(state => state.trade.type);
+  const predictionId = useAppSelector(state => state.trade.selectedOutcomeId);
+  const marketId = useAppSelector(state => state.trade.selectedMarketId);
+  const marketNetworkId = useAppSelector(
+    state => state.market.market.networkId
+  );
+
+  const portfolio = useAppSelector(state => state.polkamarkets.portfolio);
   const { login: isLoadingLogin, portfolio: isLoadingPortfolio } =
     useAppSelector(state => state.polkamarkets.isLoading);
 
@@ -36,6 +49,53 @@ function Trade({ view = 'default', onTradeFinished }: TradeProps) {
   });
 
   const config = views[view];
+
+  const outcomesWithShares = useMemo(() => {
+    if (isLoadingPortfolio) return [];
+
+    const marketShares = portfolio[marketId];
+
+    if (!marketShares) return [];
+
+    const sharesByOutcome = market.outcomes.map(outcome => {
+      const outcomeShares = marketShares.outcomes[outcome.id];
+
+      return {
+        id: outcome.id.toString(),
+        title: outcome.title,
+        shares: outcomeShares ? outcomeShares.shares : 0
+      };
+    });
+
+    return sharesByOutcome.filter(outcome => outcome.shares > 1e-5);
+  }, [isLoadingPortfolio, portfolio, marketId, market.outcomes]);
+
+  const hasSharesOfOtherOutcomes = useMemo(
+    () =>
+      !!outcomesWithShares.find(
+        outcome => outcome.id !== predictionId.toString()
+      ),
+    [outcomesWithShares, predictionId]
+  );
+
+  const prediction = useMemo(
+    () =>
+      market.outcomes.find(
+        outcome => outcome.id.toString() === predictionId.toString()
+      ),
+    [market.outcomes, predictionId]
+  );
+
+  const handleSellShares = useCallback(
+    (outcomeId: string) => {
+      if (type !== 'sell') {
+        dispatch(changeTradeType('sell'));
+      }
+
+      dispatch(selectOutcome(marketId, marketNetworkId, outcomeId));
+    },
+    [dispatch, marketId, marketNetworkId, type]
+  );
 
   if (isLoadingMarket) return null;
 
@@ -81,21 +141,46 @@ function Trade({ view = 'default', onTradeFinished }: TradeProps) {
         </div>
       ) : (
         <div className={styles.rootActions}>
-          <TradeFormInput />
-          <TradeDetails />
-          <div className={styles.actionsGroup}>
-            <TradeActions onTradeFinished={onTradeFinished} />
-            <p className={styles.terms}>
-              By clicking you’re agreeing to our{' '}
-              <a
-                href="https://www.polkamarkets.com/legal/terms-conditions"
-                target="_blank"
-                rel="noreferrer"
+          {features.fantasy.enabled &&
+          hasSharesOfOtherOutcomes &&
+          prediction ? (
+            <div className="flex-column gap-5 width-full">
+              <AlertMini
+                variant="warning"
+                description={
+                  language === 'pt'
+                    ? `Só podes escolher um resultado de cada vez. Para mudar a tua previsão para "${prediction.title}", tens que vender a tua previsão "${outcomesWithShares[0].title}".`
+                    : `You can only buy shares of one outcome at a time. In order to buy shares of "${prediction.title}" you need to sell your position of "${outcomesWithShares[0].title}".`
+                }
+                descriptionClasses="notranslate"
+              />
+              <Button
+                color="danger"
+                fullwidth
+                onClick={() => handleSellShares(outcomesWithShares[0].id)}
               >
-                Terms and Conditions
-              </a>
-            </p>
-          </div>
+                Sell
+              </Button>
+            </div>
+          ) : (
+            <>
+              <TradeFormInput />
+              <TradeDetails />
+              <div className={styles.actionsGroup}>
+                <TradeActions onTradeFinished={onTradeFinished} />
+                <p className={styles.terms}>
+                  By clicking you’re agreeing to our{' '}
+                  <a
+                    href="https://www.polkamarkets.com/legal/terms-conditions"
+                    target="_blank"
+                    rel="noreferrer"
+                  >
+                    Terms and Conditions
+                  </a>
+                </p>
+              </div>
+            </>
+          )}
         </div>
       )}
     </div>
