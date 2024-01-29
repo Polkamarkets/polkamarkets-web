a5ad6f097c465190e36e106e636c276c57b0668b
Victor <soujvnunes@gmail.com>
2024-01-29 18:59:12 -0300

    feat(ExternalJS): adjust env usage

--- src/components/ExternalJS/ExternalJS.tsx
+++ src/components/ExternalJS/ExternalJS.tsx
@@ -34,7 +34,7 @@ function ExternalJS() {
       {!!environment.CLARITY_PROJECT_ID &&
         /^[a-z0-9]+$/.test(environment.CLARITY_PROJECT_ID) && (
           <script>
-            {`(function(c,l,a,r,i,t,y){c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);})(window, document, "clarity", "script", "%REACT_APP_CLARITY_PROJECT_ID%");`}
+            {`(function(c,l,a,r,i,t,y){c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);})(window, document, "clarity", "script", "${environment.CLARITY_PROJECT_ID}");`}
           </script>
         )}
     </Helmet>
