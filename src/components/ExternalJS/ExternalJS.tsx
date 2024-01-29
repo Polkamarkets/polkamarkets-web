3f12b7c9011bbfa5756a0cea495cef20fff15269
Victor <soujvnunes@gmail.com>
2024-01-29 18:56:41 -0300

    feat(ExternalJS): conditionally render scripts based on envs

--- src/components/ExternalJS/ExternalJS.tsx
+++ src/components/ExternalJS/ExternalJS.tsx
@@ -3,11 +3,40 @@ import { Helmet } from 'react-helmet';
 import { environment } from 'config';
 
 function ExternalJS() {
-  if (!environment.EXTERNAL_JS_URL) return null;
+  const hasLocalize = environment.LOCALIZE_CONFIG?.includes('key');
 
   return (
     <Helmet>
-      <script type="text/javascript" src={environment.EXTERNAL_JS_URL} />
+      {!!environment.EXTERNAL_JS_URL && (
+        <script type="text/javascript" src={environment.EXTERNAL_JS_URL} />
+      )}
+      {!!environment.EXTERNAL_CSS_URL && (
+        <link
+          rel="stylesheet"
+          type="text/css"
+          href={environment.EXTERNAL_CSS_URL}
+        />
+      )}
+      {hasLocalize && <script type="text/javascript" src="/js/localize.js" />}
+      {hasLocalize && (
+        <script type="text/javascript">
+          {`!function(a){if(!a.Localize){a.Localize={};for(var e=["translate","untranslate","phrase","initialize","translatePage","setLanguage","getLanguage","getSourceLanguage","detectLanguage","getAvailableLanguages","untranslatePage","bootstrap","prefetch","on","off","hideWidget","showWidget"],t=0;t<e.length;t++)a.Localize[e[t]]=function(){}}}(window);`}
+        </script>
+      )}
+      {hasLocalize && (
+        <script type="text/javascript">
+          {`Localize.initialize(${environment.LOCALIZE_CONFIG});`}
+        </script>
+      )}
+      {hasLocalize && (
+        <style type="text/css">{`body #localize-powered-by,body #localize-widget.show #localize-powered-by,body #localize-widget:hover #localize-powered-by{display:none!important}`}</style>
+      )}
+      {!!environment.CLARITY_PROJECT_ID &&
+        /^[a-z0-9]+$/.test(environment.CLARITY_PROJECT_ID) && (
+          <script>
+            {`(function(c,l,a,r,i,t,y){c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);})(window, document, "clarity", "script", "%REACT_APP_CLARITY_PROJECT_ID%");`}
+          </script>
+        )}
     </Helmet>
   );
 }
