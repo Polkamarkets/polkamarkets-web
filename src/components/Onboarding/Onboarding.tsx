2a8f37959f1583cc9c26a06059fe6420e4bd13f0
Victor <soujvnunes@gmail.com>
2024-02-07 10:26:32 -0300

    feat(Onboarding): adjust initial swipe feat

--- src/components/Onboarding/Onboarding.tsx
+++ src/components/Onboarding/Onboarding.tsx
@@ -1,8 +1,7 @@
-import { useCallback, useEffect, useMemo, useState } from 'react';
+import { useCallback, useState } from 'react';
 
 import classNames from 'classnames';
-import { ui } from 'config';
-import isEmpty from 'lodash/isEmpty';
+import { AnimatePresence, motion } from 'framer-motion';
 import { Avatar } from 'ui';
 
 import { Button } from 'components/Button';
@@ -18,119 +17,138 @@ import ModalSectionText from 'components/ModalSectionText';
 import { useLocalStorage } from 'hooks';
 
 import styles from './Onboarding.module.scss';
-import { buildOnboardingSteps } from './Onboarding.utils';
+import type { OnboardingProps } from './Onboarding.type';
+import {
+  ARIA,
+  getSwipePower,
+  swipeThreshold,
+  variants,
+  wrap
+} from './Onboarding.utils';
 
-const ARIA = {
-  'aria-labelledby': 'onboarding-modal-name',
-  'aria-describedby': 'onboarding-modal-description'
-} as const;
-
-function Onboarding() {
-  const [show, setShow] = useState(false);
-  const [currentStep, setCurrentStep] = useState(0);
-
-  const [onboardingCompleted, setOnboardingCompleted] =
-    useLocalStorage<boolean>('onboardingCompleted', false);
-
-  useEffect(() => {
-    setShow(!onboardingCompleted);
-    setCurrentStep(0);
-  }, [onboardingCompleted]);
-
-  const onboardingSteps = useMemo(
-    () => buildOnboardingSteps(ui.layout.onboarding.steps),
-    []
+function Onboarding({ steps }: OnboardingProps) {
+  const [onboarding, setOnboarding] = useLocalStorage<boolean>(
+    'onboardingCompleted',
+    false
   );
+
+  const [[step, direction], setStep] = useState([0, 0]);
+
+  const isLastStep = steps.length - 1 === step;
+  const imageIndex = wrap(0, steps.length, step);
 
   const handleHide = useCallback(() => {
-    setOnboardingCompleted(true);
-    setShow(false);
-  }, [setOnboardingCompleted]);
-
-  const handleChangeStep = useCallback(
-    (step: number) => {
-      if (step > onboardingSteps.length - 1) {
-        handleHide();
-        return;
-      }
-
-      setCurrentStep(step);
+    setOnboarding(true);
+    setStep([0, 0]);
+  }, [setOnboarding]);
+  const handleStep = useCallback(
+    (newDirection: number) => {
+      if (isLastStep) handleHide();
+      else setStep([step + newDirection, newDirection]);
     },
-    [handleHide, onboardingSteps.length]
+    [handleHide, isLastStep, step]
   );
-
-  if (isEmpty(onboardingSteps)) return null;
-
-  const hasAtLeastOneImage = onboardingSteps.some(
-    ({ imageUrl }) => imageUrl !== null
-  );
-
-  const step = onboardingSteps[currentStep];
-  const isLastStep = currentStep === onboardingSteps.length - 1;
-
-  const { title, description, imageUrl } = step;
 
   return (
-    <Modal show={show} centered size="sm" onHide={handleHide}>
-      <ModalContent>
+    <Modal show={!onboarding} centered size="sm" onHide={handleHide}>
+      <ModalContent className={styles.content}>
         <ModalHeader>
           <ModalHeaderHide onClick={handleHide} />
-          <div className={styles.header}>
-            {imageUrl ? (
-              <Avatar $size="md" $radius="lg" src={imageUrl} alt={title} />
-            ) : (
-              <>
-                {hasAtLeastOneImage ? (
-                  <div style={{ height: 64, width: 64 }} />
-                ) : (
-                  <div style={{ height: 27, width: '100%' }} />
-                )}
-              </>
-            )}
-          </div>
-          <ModalHeaderTitle
-            id={ARIA['aria-labelledby']}
-            className={classNames(styles.title, 'pm-c-modal__header-title')}
-          >
-            {title}
-          </ModalHeaderTitle>
         </ModalHeader>
-        <ModalSection>
-          <ModalSectionText
-            id={ARIA['aria-describedby']}
-            className={classNames(
-              styles.description,
-              'pm-c-modal__section-description'
-            )}
+        <AnimatePresence>
+          <motion.div
+            key={step}
+            custom={direction}
+            variants={variants}
+            initial="enter"
+            animate="center"
+            exit="exit"
+            transition={{
+              x: {
+                type: 'spring',
+                stiffness: 300,
+                damping: 30
+              },
+              opacity: {
+                duration: 0.2
+              }
+            }}
+            drag="x"
+            dragConstraints={{
+              left: 0,
+              right: 0
+            }}
+            dragElastic={1}
+            onDragEnd={(_, { offset, velocity }) => {
+              const swipe = getSwipePower(offset.x, velocity.x);
+
+              if (swipe < -swipeThreshold) {
+                handleStep(1);
+              } else if (swipe > swipeThreshold) {
+                handleStep(-1);
+              }
+            }}
           >
-            {description}
-          </ModalSectionText>
-        </ModalSection>
-        <ModalFooter className={styles.footer}>
-          {onboardingSteps.length > 1 ? (
-            <div className={styles.steps}>
-              {onboardingSteps.map((item, index) => (
-                <button
-                  type="button"
-                  key={item.title}
-                  className={classNames(
-                    'pm-c-button',
-                    'pm-c-button-normal--noborder',
-                    styles.stepsItem,
-                    {
-                      [styles.stepsItemActive]: index === currentStep
-                    }
-                  )}
-                  onClick={() => setCurrentStep(index)}
+            <div className={styles.header}>
+              {steps[imageIndex].imageUrl && (
+                <Avatar
+                  $size="md"
+                  $radius="lg"
+                  src={steps[imageIndex].imageUrl}
+                  alt={steps[imageIndex].title}
                 />
-              ))}
+              )}
             </div>
-          ) : null}
+            <ModalHeaderTitle
+              id={ARIA['aria-labelledby']}
+              className={classNames(styles.title, 'pm-c-modal__header-title')}
+            >
+              {steps[imageIndex].title}
+            </ModalHeaderTitle>
+            <ModalSection>
+              <ModalSectionText
+                id={ARIA['aria-describedby']}
+                className={classNames(
+                  styles.description,
+                  'pm-c-modal__section-description'
+                )}
+              >
+                {steps[imageIndex].description}
+              </ModalSectionText>
+            </ModalSection>
+          </motion.div>
+        </AnimatePresence>
+        <ModalFooter className={styles.footer}>
+          {steps.length >= 2 && (
+            <ul className={styles.steps}>
+              {steps.map((item, index) => (
+                <li key={item.title} className={styles.stepsItem}>
+                  <button
+                    type="button"
+                    aria-label={`Go to step ${index + 1}`}
+                    className={classNames(styles.stepsItemButton, {
+                      [styles.stepsItemButtonActive]: index === step
+                    })}
+                    onClick={() => {
+                      handleStep(index > step ? 1 : -1);
+                      /*  if (index > step) {
+                          handleStep(1); 
+                        } else if (index < step) {
+                          handleStep(-1);
+                        } */
+                    }}
+                  />
+                </li>
+              ))}
+            </ul>
+          )}
           <Button
             size="sm"
             color="primary"
             fullwidth
-            onClick={() => handleChangeStep(currentStep + 1)}
+            onClick={() => {
+              handleStep(1);
+            }}
           >
             {isLastStep ? "Let's Go" : 'Next'}
           </Button>
