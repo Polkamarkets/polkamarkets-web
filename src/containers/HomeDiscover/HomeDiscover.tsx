ae24a26b9c266cdd61e54f5c103fd010ca318fab
Wellington Matheus <w.matheus@outlook.com>
2024-03-08 10:45:15 -0300

    feat(Containers): create `HomeDiscover`

---
+++ src/containers/HomeDiscover/HomeDiscover.tsx
@@ -0,0 +1,11 @@
+import styles from './HomeDiscover.module.scss';
+
+function HomeDiscover() {
+  return (
+    <div className={styles.root}>
+      <h2 className={styles.title}>Discover</h2>
+    </div>
+  );
+}
+
+export default HomeDiscover;
--- src/containers/index.ts
+++ src/containers/index.ts
@@ -1,2 +1,2 @@
-/* eslint-disable import/prefer-default-export */
 export { default as HomeBanner } from './HomeBanner';
+export { default as HomeDiscover } from './HomeDiscover';
