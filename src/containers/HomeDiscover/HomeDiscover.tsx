b88cb8e7dbd2f3f21f369e3a46f82d0083bc64d3
Wellington Matheus <w.matheus@outlook.com>
2024-03-09 10:27:19 -0300

    feat(HomeDiscover): add tabs

--- src/containers/HomeDiscover/HomeDiscover.tsx
+++ src/containers/HomeDiscover/HomeDiscover.tsx
@@ -1,9 +1,25 @@
+import { Tabs, TabList, Tab, TabPanel } from 'react-aria-components';
+
 import styles from './HomeDiscover.module.scss';
 
 function HomeDiscover() {
   return (
     <div className={styles.root}>
-      <h2 className={styles.title}>Discover</h2>
+      <div className={styles.header}>
+        <h2 className={styles.title}>Discover</h2>
+      </div>
+      <Tabs className={styles.tabs}>
+        <TabList className={styles.tabList} aria-label="Discover">
+          <Tab className={styles.tab} id="lands">
+            Lands
+          </Tab>
+          <Tab className={styles.tab} id="contests">
+            Contests
+          </Tab>
+        </TabList>
+        <TabPanel className={styles.tabPanel} id="lands" />
+        <TabPanel className={styles.tabPanel} id="contests" />
+      </Tabs>
     </div>
   );
 }
