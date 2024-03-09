7ff0fba332db298401e1c9a1833f2e3b759ffabb
Wellington Matheus <w.matheus@outlook.com>
2024-03-08 23:25:37 -0300

    feat(Containers): create `CommunityCard`

---
+++ src/containers/CommunityCard/CommunityCard.tsx
@@ -0,0 +1,73 @@
+import { useCallback, useMemo } from 'react';
+import { Link } from 'react-router-dom';
+
+import formatImageAlt from 'helpers/formatImageAlt';
+import { Land } from 'types/land';
+import { Avatar } from 'ui';
+
+import styles from './CommunityCard.module.scss';
+
+type CommunityCardProps = {
+  land: Land;
+};
+
+function CommunityCard({ land }: CommunityCardProps) {
+  const avatarAltFormatter = useCallback(formatImageAlt, []);
+
+  const stats = useMemo(() => {
+    const members = land.users || 0;
+    const contests = land.tournaments.length;
+    const questions = land.tournaments
+      .map(tournament => tournament.markets?.length || 0)
+      .reduce((a, b) => a + b, 0);
+
+    return {
+      members,
+      contests,
+      questions
+    };
+  }, [land.tournaments, land.users]);
+
+  return (
+    <Link to={`/${land.slug}`} className={styles.root}>
+      <div
+        className={styles.banner}
+        style={
+          {
+            '--background-image': `url(${land.bannerUrl})`
+          } as React.CSSProperties
+        }
+      />
+      <div className={styles.info}>
+        <Avatar
+          $size="xs"
+          $radius="lg"
+          src={!(land.imageUrl === null) ? land.imageUrl : undefined}
+          alt={land.title}
+          altFormatter={avatarAltFormatter}
+          className={styles.infoAvatar}
+          fallbackClassName={styles.infoAvatarFallback}
+        />
+        <h3 className={styles.infoName}>{land.title}</h3>
+      </div>
+      <div className={styles.footer}>
+        <div className={styles.footerStats}>
+          <span className={styles.footerStatsItem}>
+            Members: <strong className="notranslate">{stats.members}</strong>
+          </span>
+          <span className={styles.divider} />
+          <span className={styles.footerStatsItem}>
+            Contests: <strong className="notranslate">{stats.contests}</strong>
+          </span>
+          <span className={styles.divider} />
+          <span className={styles.footerStatsItem}>
+            Questions:{' '}
+            <strong className="notranslate">{stats.questions}</strong>
+          </span>
+        </div>
+      </div>
+    </Link>
+  );
+}
+
+export default CommunityCard;
--- src/containers/index.ts
+++ src/containers/index.ts
@@ -1,2 +1,3 @@
 export { default as HomeBanner } from './HomeBanner';
 export { default as HomeDiscover } from './HomeDiscover';
+export { default as CommunityCard } from './CommunityCard';
