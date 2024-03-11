a24c8ac90994446f81cfbd7728154974ff9bfd35
Wellington Matheus <w.matheus@outlook.com>
2024-03-11 07:44:55 -0300

    feat(Containers): create `Contests`

---
+++ src/containers/Contests/Contests.tsx
@@ -0,0 +1,34 @@
+import { environment } from 'config';
+import ContestCard from 'containers/ContestCard';
+import { useGetTournamentsQuery } from 'services/Polkamarkets';
+
+import styles from './Contests.module.scss';
+
+function Contests() {
+  const {
+    data: tournaments,
+    isFetching,
+    isLoading
+  } = useGetTournamentsQuery({
+    token: environment.FEATURE_FANTASY_TOKEN_TICKER
+  });
+
+  const isLoadingGetTournamentsQuery = isFetching || isLoading;
+
+  if (isLoadingGetTournamentsQuery)
+    return (
+      <div className="flex-row justify-center align-center width-full padding-y-5 padding-x-4">
+        <span className="spinner--primary" />
+      </div>
+    );
+
+  return (
+    <div className={styles.root}>
+      {tournaments?.map(tournament => (
+        <ContestCard tournament={tournament} key={tournament.id} />
+      ))}
+    </div>
+  );
+}
+
+export default Contests;
--- src/containers/index.ts
+++ src/containers/index.ts
@@ -3,3 +3,4 @@ export { default as HomeDiscover } from './HomeDiscover';
 export { default as CommunityCard } from './CommunityCard';
 export { default as ContestCard } from './ContestCard';
 export { default as Communities } from './Communities';
+export { default as Contests } from './Contests';
