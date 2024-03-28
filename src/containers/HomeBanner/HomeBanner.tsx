0ee76ccd59a66a5ec479efa64ed6cbe2b5092eea
Wellington Matheus <w.matheus@outlook.com>
2024-03-28 08:28:23 -0300

    feat(HomeBanner): add mobile breakpoints and loggedIn state

--- src/containers/HomeBanner/HomeBanner.tsx
+++ src/containers/HomeBanner/HomeBanner.tsx
@@ -1,17 +1,24 @@
 import { Button } from 'ui';
 
+import { useAppSelector } from 'hooks';
+
 import styles from './HomeBanner.module.scss';
 
 function HomeBanner() {
+  const isLoggedIn = useAppSelector(state => state.polkamarkets.isLoggedIn);
+
   return (
     <div className={styles.root}>
       <div className={styles.content}>
         <h1 className={styles.title}>Predict, Play, and Collect Rewards.</h1>
         <p className={styles.description}>
-          Unleash the Power of your Predictions. Turn insights into real prizes.
+          Unleash the Power of your Predictions.
         </p>
+        <p className={styles.description}>Turn insights into real prizes.</p>
       </div>
-      <Button size="lg">Start Playing</Button>
+      <Button size="lg">
+        {isLoggedIn ? 'Start Playing' : 'Sign up to Play'}
+      </Button>
     </div>
   );
 }
